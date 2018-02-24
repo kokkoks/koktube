fa2111d2811af8eb303275f9e2557b591e731af2
Suttanan Charoenpanich <kok.suttanan@gmail.com>
2018-02-24 10:04:56 +0700

    Create Project

---
+++ KokTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KokTube
+//
+//  Created by Suttanan Charoenpanich on 24/2/2561 BE.
+//  Copyright © 2561 Suttanan Charoenpanich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+}
+
